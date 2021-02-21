8310ad5b7a4599d3f4642de97ab2b6b75de0c5e1
seongho.lee <supermania2619@gmail.com>
2021-02-21 18:14:32 +0900

    setting Initial work

--- main.c
+++ main.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
 
 int main(){
 
 	//1차 서류심사
 	//2차 면접시험
 	//서류심사 성적과 면접 시험 성적 중 적어도 하나가 다른 지원보다 떨어지지 않는 자만 선발
-	
-	
-	printf("hello World!\n");
+	int T;
+
+	scanf("%d",&T); //테스트 케이스의 개수(1 <= T <= 20)
+
+	while(T){
+		int **data;
+		int i,j;
+		int N;
+
+		scanf("%d",&N);
+
+		//START : Init Data Array
+		data = (int**)malloc(sizeof(int*)*N);
+		for(i=0; i<N; i++)
+			data[i] = (int*)malloc(sizeof(int)*2);
+		
+		for(i=0; i<N; i++)
+			for(j=0; j<2; j++)
+				data[i][j] = 0;
+		//END : Init Data Array
+
+		for(i=0; i<N; i++)
+			scanf("%d %d",data[i][0], data[i][1]);
+		
+		//START : deInit Data Array
+		for(i=0; i<N; i++)
+			free(data[i]);
+		free(data);
+		//END : deInit Data Array
+		
+		T--;
+	}
+
 	return 0;
 }
