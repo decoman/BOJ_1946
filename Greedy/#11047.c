ef57cf77c77c1575479d7d454a9f955cbb045c13
seongho.lee <supermania2619@gmail.com>
2021-02-21 22:31:58 +0900

    final pass code

--- Greedy/#11047.c
+++ Greedy/#11047.c
@@ -3,9 +3,11 @@
 
 
 int main(){
-    int N,K; //N: 동전의 종류, K: 만드려는 가치의 합
+    int N,K; //N: 동전의 종류 (1 <= N <= 10)
+             //K: 만드려는 가치의 합 (1 <= K <= 100,000,000), A[0] = 1, index가 2 이상인 경우 이전 값의 배수
     int *coin;
-    int i;
+    int i,r;
+    int count = 0;
 
     scanf("%d %d",&N, &K);
 
@@ -14,10 +16,17 @@ int main(){
     for(i=0; i<N; i++)
         scanf("%d",&coin[i]);
 
-    for(i=0; i<N; i++)
-        printf("%d\n",coin[i]);
+    while(K != 0 && N > 0)
+    {
+        if(coin[N-1] <= K)
+        {
+            count += K/coin[N-1];
+            K %= coin[N-1];
+        }
+        N--;
+    }
 
-
+    printf("%d",count);
 
     return 0;
 }
